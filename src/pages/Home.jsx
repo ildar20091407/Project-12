eae824342e128cea8e1ea076a62af97844931442
Ildar <I;darlaishev2@gmail.com>
2025-07-14 14:11:33 +0500

    5

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useEffect, useState} from 'react';
 import { useTranslation } from 'react-i18next';
 import emailjs from '@emailjs/browser';
 import photo from '../assets/images/photo_2025-02-10_09-43-29.jpg';
@@ -7,27 +7,60 @@ import s from "./Home.module.scss";
 
 export const Home = () => {
     const { t } = useTranslation();
+    const footerRef = useRef(null);
+    const [isFooterVisible, setIsFooterVisible] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        setIsFooterVisible(true);
+                    } else {
+                        setIsFooterVisible(false);
+                    }
+                });
+            },
+            {
+                root: null,
+                rootMargin: '0px',
+                threshold: 0.5,
+            }
+        );
+
+        const currentFooterRef = footerRef.current;
+
+        if (currentFooterRef) {
+            observer.observe(currentFooterRef);
+        }
+
+        return () => {
+            if (currentFooterRef) {
+                observer.unobserve(currentFooterRef);
+            }
+        };
+    }, [footerRef]);
 
     return (
         <>
-         <nav className="nav__bottom">
-                        <img src={background} className="background" alt="" />
-                        <div className="nav__botom-box">
-                            <div className="main__top-right">
-                                <h2 className="nav__title">{t('Hello!')}</h2>
-                                <h2 className="nav__title">{t('I am')} </h2>
-                                <p className="nav__text">{t('Web-development')}</p>
-                            </div>
-                        </div>
-                    </nav>
+            <nav className="nav__bottom">
+                <img src={background} className="background" alt="" />
+                <div className="nav__botom-box">
+                    <div className="main__top-right">
+                        <h2 className="nav__title">{t('Hello!')}</h2>
+                        <h2 className="nav__title">{t('I am')} </h2>
+                        <p className="nav__text">{t('Web-development')}</p>
+                    </div>
+                </div>
+            </nav>
             <main className={s.main__top}>
                 <div className={s.main__top_title}>
                     <p className={s.main__top_title}>{t('aboutMe')}</p>
                 </div>
-                <div className={s.container}>
+                <div className="container">
                     <div className={s.main__top_box}>
                         <div className={s.main__top_left}>
-                            <img className={s.main__top_left_img} src={photo} />
+                            <img className={s.main__top_left_img} src={photo} alt="Photo" />
                         </div>
                         <div className={s.main__top_right}>
                             <h2 className={s.main__top_right_title}>{t('iAm')} {t('name')}</h2>
@@ -40,47 +73,43 @@ export const Home = () => {
                 </div>
             </main>
             <div className="container">
-
-            <footer className={s.footer}>
-                <div className={s.footer__box}>
-                    <h2 className={s.footer__box_title}>Мои навыки</h2>
-                </div>
-                
-                <div className={s.footer__main}>
-                    <div className={s.footer__main_item}>
-                        <p className={s.footer__main_item_text}>HTML</p>
-                         <div className={s.footer__main_item_number}>
-                            <p className={s.footer__main_item_color1}>45%</p>
-                         </div>
+                    <div className={s.footer__box}>
+                        <h2 className={s.footer__box_title}>Мои навыки</h2>
+                    </div>
+                <footer className={`${s.footer} ${isFooterVisible ? s.footerVisible : ''}`} ref={footerRef}>
+                    <div className={s.footer__main}>
+                        <div className={s.footer__main_item}>
+                            <p className={s.footer__main_item_text}>HTML</p>
+                            <div className={s.footer__main_item_number}>
+                                <p className={s.footer__main_item_color1}>80%</p>
+                            </div>
+                        </div>
+                        <div className={s.footer__main_item}>
+                            <p className={s.footer__main_item_text}>CSS</p>
+                            <div className={s.footer__main_item_number}>
+                                <p className={s.footer__main_item_color2}>75%</p>
+                            </div>
+                        </div>
+                        <div className={s.footer__main_item}>
+                            <p className={s.footer__main_item_text}>Java Script</p>
+                            <div className={s.footer__main_item_number}>
+                                <p className={s.footer__main_item_color3}>80%</p>
+                            </div>
+                        </div>
+                        <div className={s.footer__main_item}>
+                            <p className={s.footer__main_item_text}>React</p>
+                            <div className={s.footer__main_item_number}>
+                                <p className={s.footer__main_item_color4}>85%</p>
+                            </div>
+                        </div>
                     </div>
                     <div className={s.footer__main_item}>
-                        <p className={s.footer__main_item_text}>HTML</p>
-                         <div className={s.footer__main_item_number}>
-                            <p className={s.footer__main_item_color2}>45%</p>
-                         </div>
+                        <p className={s.footer__main_item_text}>Type Script</p>
+                        <div className={s.footer__main_item_number}>
+                            <p className={s.footer__main_item_color5}>55%</p>
+                        </div>
                     </div>
-                    <div className={s.footer__main_item}>
-                        <p className={s.footer__main_item_text}>HTML</p>
-                         <div className={s.footer__main_item_number}>
-                            <p className={s.footer__main_item_color3}>45%</p>
-                         </div>
-                    </div>
-                    <div className={s.footer__main_item}>
-                        <p className={s.footer__main_item_text}>HTML</p>
-                         <div className={s.footer__main_item_number}>
-                            <p className={s.footer__main_item_color4}>45%</p>
-                         </div>
-                    </div>
-                </div>
-                    <div className={s.footer__main_item}>
-                        <p className={s.footer__main_item_text}>HTML</p>
-                         <div className={s.footer__main_item_number}>
-                            <p className={s.footer__main_item_color5}>45%</p>
-                         </div>
-                    </div>
-
-                    
-            </footer>
+                </footer>
             </div>
         </>
     );
@@ -151,3 +180,5 @@ const CombinedComponent = () => {
 
 
 export default CombinedComponent;
+
+
--- src/pages/Home1.jsx
+++ src/pages/Home1.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import emailjs from '@emailjs/browser';
 import photo from '../assets/images/photo_2025-02-10_09-43-29.jpg';
@@ -7,30 +7,64 @@ import background from '../assets/images/tild3163-6265-4137-a337-643863353964__3
 
 export const Home = () => {
     const { t } = useTranslation();
+    const footerRef = useRef(null);
+    const [isFooterVisible, setIsFooterVisible] = useState(false);
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            (entries) => {
+                entries.forEach(entry => {
+                    if (entry.isIntersecting) {
+                        setIsFooterVisible(true);
+                    } else {
+                        setIsFooterVisible(false);
+                    }
+                });
+            },
+            {
+                root: null,
+                rootMargin: '0px',
+                threshold: 0.5,
+            }
+        );
+
+        const currentFooterRef = footerRef.current;
+
+        if (currentFooterRef) {
+            observer.observe(currentFooterRef);
+        }
+
+        return () => {
+            if (currentFooterRef) {
+                observer.unobserve(currentFooterRef);
+            }
+        };
+    }, [footerRef]);
 
     return (
         <>
-         <nav className="nav__bottom">
-                        <img src={background} className="background" alt="" />
-                        <div className="nav__botom-box">
-                            <div className="main__top-right">
-                                <h2 className="nav__title">{t('Hello!')}</h2>
-                                <h2 className="nav__title">{t('I am')} </h2>
-                                <p className="nav__text">{t('Web-development1')}</p>
-                            </div>
-                        </div>
-                    </nav>        
+            <nav className="nav__bottom">
+                <img src={background} className="background" alt="" />
+                <div className="nav__botom-box">
+                    <div className="main__top-right">
+                        <h2 className="nav__title">{t('Hello!')}</h2>
+                        <h2 className="nav__title">{t('I am')} </h2>
+                        <p className="nav__text">{t('Web-development1')}</p>
+                    </div>
+                </div>
+            </nav>
             <main className={s.main__top}>
                 <div className={s.main__top_title}>
-                    <p className={s.main__top_title}>{t('aboutMe1')}</p>
+                    <p className={s.main__top_title}>{t('aboutMe')}</p>
                 </div>
-                <div className={s.container}>
+                <div className="container">
                     <div className={s.main__top_box}>
                         <div className={s.main__top_left}>
-                            <img className={s.main__top_left_img} src={photo} />
+                            <img className={s.main__top_left_img} src={photo} alt="Photo" />
                         </div>
                         <div className={s.main__top_right}>
-                            <h2 className={s.main__top_right_title}>{t('iAm1')} {t('name1')}</h2>
+                            <h2 className={s.main__top_right_title}>{t('iAm')} {t('name')}</h2>
+                            <p className={s.main__top_right_text}>{t('frontEndDeveloper')}</p>
                             <p className={s.main__top_right_text}>
                                 {t('description1')}
                             </p>
@@ -38,6 +72,45 @@ export const Home = () => {
                     </div>
                 </div>
             </main>
+            <div className="container">
+                    <div className={s.footer__box}>
+                        <h2 className={s.footer__box_title}>Мои навыки</h2>
+                    </div>
+                <footer className={`${s.footer} ${isFooterVisible ? s.footerVisible : ''}`} ref={footerRef}>
+                    <div className={s.footer__main}>
+                        <div className={s.footer__main_item}>
+                            <p className={s.footer__main_item_text}>HTML</p>
+                            <div className={s.footer__main_item_number}>
+                                <p className={s.footer__main_item_color1}>80%</p>
+                            </div>
+                        </div>
+                        <div className={s.footer__main_item}>
+                            <p className={s.footer__main_item_text}>CSS</p>
+                            <div className={s.footer__main_item_number}>
+                                <p className={s.footer__main_item_color2}>75%</p>
+                            </div>
+                        </div>
+                        <div className={s.footer__main_item}>
+                            <p className={s.footer__main_item_text}>Java Script</p>
+                            <div className={s.footer__main_item_number}>
+                                <p className={s.footer__main_item_color3}>80%</p>
+                            </div>
+                        </div>
+                        <div className={s.footer__main_item}>
+                            <p className={s.footer__main_item_text}>React</p>
+                            <div className={s.footer__main_item_number}>
+                                <p className={s.footer__main_item_color4}>85%</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={s.footer__main_item}>
+                        <p className={s.footer__main_item_text}>Type Script</p>
+                        <div className={s.footer__main_item_number}>
+                            <p className={s.footer__main_item_color5}>55%</p>
+                        </div>
+                    </div>
+                </footer>
+            </div>
         </>
     );
 };
@@ -59,7 +132,7 @@ const ContactForm = () => {
             });
     };
 
-   return (
+    return (
 
         <nav className="footer">
 
@@ -107,3 +180,5 @@ const CombinedComponent = () => {
 
 
 export default CombinedComponent;
+
+
